74cd4ece7d9ed8a6e6e5523d0dc0d67d98d75903
Ryan O'Connor <72693746+TheOneTrueNoodle@users.noreply.github.com>
2022-12-11 12:57:04 +0000

    Work on the physics of explosion
    A decent portion of the work involved in developing the physics interaction of the exploding food. Still needs more work.

---
+++ Retro_SciFi_Microwave/Assets/Ryans Stuff/Scripts/Explode.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour
+{
+    public List<GameObject> pieces;
+
+    private bool Exploded;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void explode(float force, float radius, float upward)
+    {
+        if (Exploded) { return; }
+
+        foreach(GameObject obj in pieces)
+        {
+            Rigidbody newrb = obj.GetComponent<Rigidbody>();
+            newrb.isKinematic = false;
+            Vector3 direction = Random.insideUnitSphere;
+            newrb.AddForce(direction.normalized * force);
+
+            obj.GetComponent<Collider>().enabled = true;
+        }
+
+        rb.AddExplosionForce(force, transform.position, radius, upward);
+        Exploded = true;
+    }
+}
