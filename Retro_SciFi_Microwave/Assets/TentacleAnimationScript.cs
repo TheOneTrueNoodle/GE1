1c8da8c2875c349128f66604734b561b7204a63e
Squange <78355315+SquangeEj@users.noreply.github.com>
2022-12-19 20:37:42 +0000

    Added Tentacle IK
    The inverse kinematics on the tentacle allow for procedural animations

---
+++ Retro_SciFi_Microwave/Assets/TentacleAnimationScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TentacleAnimationScript : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
