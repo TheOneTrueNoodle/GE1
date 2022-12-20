69f398e6c63a6fe1d9453e2b9f287474d114b4e9
Squange <78355315+SquangeEj@users.noreply.github.com>
2022-12-20 19:04:08 +0000

    Added dream music
    it is my favourite song so i wanted it in the rave :)

--- Retro_SciFi_Microwave/Assets/InteractScript.cs
+++ Retro_SciFi_Microwave/Assets/InteractScript.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class InteractScript : MonoBehaviour
 {
+    
     [SerializeField] private MonoBehaviour Script;
     public void Activated()
     {
+
         Script.StartCoroutine("Activate"); 
+
     }
 }
--- Retro_SciFi_Microwave/Assets/RaveScript.cs
+++ Retro_SciFi_Microwave/Assets/RaveScript.cs
@@ -6,7 +6,7 @@ public class RaveScript : MonoBehaviour
 {
     
 
-    // Update is called once per frame
+    
     void Update()
     {
         transform.Rotate(1, 1.5f, 0.5f);
